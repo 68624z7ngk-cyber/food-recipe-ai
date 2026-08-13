d3cf22d21ad3f6632a46e6b39504ec1060fe9eb2
test <test>
2026-08-13 23:38:10 +0900

    Fix photo app login URL

--- config/settings.py
+++ config/settings.py
@@ -159,7 +159,7 @@ MAILERS = {
 # MEDIA_URL = "/media/"
 # MEDIA_ROOT = BASE_DIR / "media"
 
-LOGIN_URL = "/login/"
+LOGIN_URL = "/photos/login/"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/login/"
 
